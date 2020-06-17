33bc55c1a9ec1d6e200f988983164745640d9197
Pablo Anglat <pablo.anglat@globant.com>
2020-06-17 08:58:10 -0300

    Updates

---
+++ precache-manifest.292b663b8bd8b6ab7cd2518641968e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d280d8a8f007551d813030cdf8aa89ed",
+    "url": "/code-challenge-forecast/index.html"
+  },
+  {
+    "revision": "77baea6b8f29aa7c2b51",
+    "url": "/code-challenge-forecast/static/css/main.a89becec.chunk.css"
+  },
+  {
+    "revision": "2ec6a80b70cc22ece2eb",
+    "url": "/code-challenge-forecast/static/js/2.5733cbd1.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/code-challenge-forecast/static/js/2.5733cbd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77baea6b8f29aa7c2b51",
+    "url": "/code-challenge-forecast/static/js/main.ff959b87.chunk.js"
+  },
+  {
+    "revision": "bd14f86367c003afe09e",
+    "url": "/code-challenge-forecast/static/js/runtime-main.fb0dee54.js"
+  }
+]);
